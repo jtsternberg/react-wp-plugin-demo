44ea242c7e265f57ac9f4228ea343be1aa316e08
Justin Sternberg <justin@dsgnwrks.pro>
2018-04-30 15:38:52 -0400

    WP code standards

--- index.php
+++ index.php
@@ -11,22 +11,24 @@ Author URI: http://www.11online.us
 */
 
 // add our content filter
-add_filter('the_content', 'add_react_reviews_to_posts');
+add_filter( 'the_content', 'add_react_reviews_to_posts' );
 
-function add_react_reviews_to_posts($content) {
+function add_react_reviews_to_posts( $content ) {
 
 	// if this is a post, but not the blog page
-	if(is_single() && !is_home()) {
+	if ( is_single() && ! is_home() ) {
 		// get our post id for the api
-		$post_id = get_the_ID();
-		$user_id = get_current_user_id();
+		$post_id  = get_the_ID();
+		$user_id  = get_current_user_id();
 		$rest_url = get_rest_url();
 		// add our content, a div with id of root for our react script
 		$content .= "<div id='root' data-id='$post_id' data-user='$user_id' data-resturl='$rest_url'></div>";
 
-		$files = array_filter( glob( __DIR__ . '/wp-plugin/build/static/js/*.js' ), function( $file ) {
-			return false !== strpos( $file, 'js/main.' );
-		} );
+		$files = array_filter(
+			glob( __DIR__ . '/wp-plugin/build/static/js/*.js' ), function( $file ) {
+				return false !== strpos( $file, 'js/main.' );
+			}
+		);
 		$file = end( $files );
 		$file = plugins_url( str_replace( __DIR__, '', $file ), __FILE__ );
 
@@ -39,54 +41,66 @@ function add_react_reviews_to_posts($content) {
 
 
 // add our api add review action
-add_action( 'rest_api_init', function () {
-	register_rest_route( 'reviews/v1', '/add-review', array(
-		'methods' => 'POST',
-		'callback' => 'add_review_route'
-	) );
-} );
+add_action(
+	'rest_api_init', function () {
+		register_rest_route(
+			'reviews/v1', '/add-review', array(
+				'methods'  => 'POST',
+				'callback' => 'add_review_route',
+			)
+		);
+	}
+);
 
-function add_review_route($request) {
-	if (isset($request['data'])) {
+function add_review_route( $request ) {
+	if ( isset( $request['data'] ) ) {
 		$user_id = $request['data']['user_id'];
 		$post_id = $request['data']['post_id'];
-		$review = $request['data']['review'];
-		$key = 'review_of_post_' . $post_id;
-		update_user_meta($user_id, $key, $review);
+		$review  = $request['data']['review'];
+		$key     = 'review_of_post_' . $post_id;
+		update_user_meta( $user_id, $key, $review );
 	}
-	return get_reviews_by_post($post_id);
+	return get_reviews_by_post( $post_id );
 }
 
 // add our api get reviews action
-add_action( 'rest_api_init', function () {
-	register_rest_route( 'reviews/v1', '/get-reviews/(?P<id>\d+)', array(
-		'methods' => 'GET',
-		'callback' => 'get_reviews_by_post_route'
-	) );
-} );
+add_action(
+	'rest_api_init', function () {
+		register_rest_route(
+			'reviews/v1', '/get-reviews/(?P<id>\d+)', array(
+				'methods'  => 'GET',
+				'callback' => 'get_reviews_by_post_route',
+			)
+		);
+	}
+);
 
-function get_reviews_by_post_route($data) {
-	return get_reviews_by_post($data['id']);
+function get_reviews_by_post_route( $data ) {
+	return get_reviews_by_post( $data['id'] );
 }
 
 // this is our get reviews helper function
-function get_reviews_by_post($post_id) {
+function get_reviews_by_post( $post_id ) {
 
 	$meta_key = 'review_of_post_' . $post_id;
 
 	$users = get_users(
 		[
 			'meta_key' => $meta_key,
-			'fields' => ['ID', 'user_nicename']
+			'fields'   => [ 'ID', 'user_nicename' ],
 		]
 	);
 
 	$userArray = [];
 	// I know this isn't efficient, but for our demo it will suffice. In production, you'd want to write a join to get all the data at once.
-	foreach($users as $user) {
-		$user_meta = get_user_meta ( $user->ID, $meta_key, true);
-		$image = get_avatar_url($user->ID, ['size' => 36]);
-		$userArray[$user->ID] = ['review' => $user_meta, 'image' => $image, 'name' => $user->user_nicename];
+	foreach ( $users as $user ) {
+		$user_meta              = get_user_meta( $user->ID, $meta_key, true );
+		$image                  = get_avatar_url( $user->ID, [ 'size' => 36 ] );
+		$userArray[ $user->ID ] = [
+			'review' => $user_meta,
+			'image'  => $image,
+			'name'   => $user->user_nicename,
+		];
 	}
 
 	return $userArray;
